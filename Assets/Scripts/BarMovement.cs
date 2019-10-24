28ff7f49d8e68523d4d64bc5df6505b88c61b2e7
LEONARDO ALMEIDA BEVILACQUA <1510033187@acad.dvuadmin.net>
2019-10-24 19:18:52 -0300

    moving the bar

--- Assets/Scripts/BarMovement.cs
+++ Assets/Scripts/BarMovement.cs
@@ -8,25 +8,32 @@ public class BarMovement : MonoBehaviour
     private Vector2 velocity;
     public Rigidbody2D rb2d;
 
+    public float barVelocity = 1.75f;
+
     // Start is called before the first frame update
     void Start()
     {
-        velocity = new Vector2(1.75f, 0.0f);
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        int left = Input.GetKey(KeyCode.LeftArrow) ? -1 : 0;
+        int right = Input.GetKey(KeyCode.RightArrow) ? 1 : 0;
+
+        velocity = new Vector2((barVelocity * (left + right)), 0.0f);
+
         rb2d.MovePosition(rb2d.position + velocity * Time.fixedDeltaTime);
     }
 
     // OnTriggerEnter is called when a collision is trigger
-     void OnTriggerEnter(Collider other) 
-     {
-         
-         if (other.gameObject.CompareTag ("Wall"))
-         {
-             other.gameObject.SetActive (false);
-         }
-     }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            velocity = new Vector2(0.0f, 0.0f);
+        }
+    }
 }
